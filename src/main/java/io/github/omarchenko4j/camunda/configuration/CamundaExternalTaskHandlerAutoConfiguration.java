c2b3c3b6ed85c92c9a600b84b086972b6a572ac7
Oleg Marchenko <omarchenko.150@gmail.com>
2021-10-03 22:44:27 +0300

    Create skeleton for Spring Boot Starter

---
+++ src/main/java/io/github/omarchenko4j/camunda/configuration/CamundaExternalTaskHandlerAutoConfiguration.java
@@ -0,0 +1,10 @@
+package io.github.omarchenko4j.camunda.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Oleg Marchenko
+ */
+@Configuration
+public class CamundaExternalTaskHandlerAutoConfiguration {
+}
